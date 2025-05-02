a4ff548d5df317dabc0d00d8bfd143c8610a398f
DevAidan <aidanhumpidge333@gmail.com>
2025-05-02 18:22:50 +0100

    homepage completed

---
+++ travelapp/lib/components/nav_bar_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class NavBarButton extends StatelessWidget {
+  final iconName;
+  bool isSelected;
+  NavBarButton({
+    required this.iconName,
+    required this.isSelected,
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Container(
+      decoration: BoxDecoration(
+        color: isSelected ? theme.colorScheme.primary : theme.colorScheme.secondary,
+        borderRadius: BorderRadius.circular(32)
+      ),
+      child: IconButton(
+        onPressed: (){}, 
+        icon: Icon(
+          iconName,
+          color: isSelected ? theme.colorScheme.secondary.withValues(alpha: 0.9) : theme.colorScheme.primary.withValues(alpha: 0.9),
+          size: 40,
+        )
+      ),
+    );
+  }
+}
--- travelapp/lib/pages/home_page.dart
+++ travelapp/lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:travelapp/components/card_view.dart';
 import 'package:travelapp/components/country_filter.dart';
 import 'package:travelapp/components/heading_tile.dart';
+import 'package:travelapp/components/nav_bar_button.dart';
 import 'package:travelapp/components/search_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,8 +10,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     return Scaffold(
-      //Bottom App Bar
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
         child: Column(
@@ -21,12 +22,12 @@ class HomePage extends StatelessWidget {
               userImagePath: "lib/assets/profileT.jpg"
             ),
 
-            SizedBox(height: 20),
+            SizedBox(height: 25),
 
             //Search Bar
             MySearchBar(),
 
-            SizedBox(height: 20),
+            SizedBox(height: 25),
         
             //Country selection
             Align(
@@ -40,16 +41,52 @@ class HomePage extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 20),
+            SizedBox(height: 25),
 
             CountryFilter(
               countries: ["Asia", "Europe", "South America", "North America"]
             ),
 
-            SizedBox(height: 20),
+            SizedBox(height: 40),
 
             //Image viewer
-            CardView()
+            CardView(),
+
+            SizedBox(height: 40),
+
+            //Nav Bar
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 280,
+              decoration: BoxDecoration(
+                color: theme.colorScheme.secondary,
+                borderRadius: BorderRadius.circular(50)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  NavBarButton(
+                    iconName: Icons.home_outlined, 
+                    isSelected: true
+                  ),
+                  SizedBox(width: 10),
+                  NavBarButton(
+                    iconName: Icons.article_outlined, 
+                    isSelected: false
+                  ),
+                  SizedBox(width: 10),
+                  NavBarButton(
+                    iconName: Icons.favorite_outline, 
+                    isSelected: false
+                  ),
+                  SizedBox(width: 10),
+                  NavBarButton(
+                    iconName: Icons.apps_outlined, 
+                    isSelected: false
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
